91e85c400c81f3b6d2c244a274e9281ff54e6a0d
Thanniru Sai Teja <168437838+iam-saiteja@users.noreply.github.com>
2026-08-17 03:27:40 +0530

    feat: wire company branding and attribution

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
+import './company-branding.css';
 import { startSupportPersistence } from './lib/supportPersistence';
+import { initCompanyBranding } from './companyBranding';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -14,3 +16,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 );
 
 startSupportPersistence();
+initCompanyBranding();
